b5d06fae1c52bb3628ad36bdb441b498fb331a15
Rahul Datta <rahuldatta@caspia.local>
2014-05-29 16:51:19 -0700

    Added controllers for home and hackday pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'dynamic_pages/home'
+
+  get 'dynamic_pages/hackday'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
