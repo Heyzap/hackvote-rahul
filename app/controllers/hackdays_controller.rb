9fe41719e4fe26030f22620a0cc9672b54e4f5c4
rdatta11 <rdatta11@gmail.com>
2014-06-03 00:12:13 -0700

    Started working on sessions and vote counts

--- app/controllers/hackdays_controller.rb
+++ app/controllers/hackdays_controller.rb
@@ -20,8 +20,35 @@ class HackdaysController < ApplicationController
 		@hackday = Hackday.find(params[:id])
 	end
 
+	def update
+		@hackday = Hackday.find(params[:id])
+		if params[:radioButton]
+			@hack = @hack.find_by_id(params[:radioButton])
+			if get_num_votes(@hackday) > 0
+				decrease_votes(@hackday)
+				@hack.votes = @hack.votes + 1
+				@hack.save
+			else
+				flash[:error] = "Error. You have no votes left"
+			end
+		end
+		redirect_to hackday_hacks_path
+	end
+
 	private
+
+	NUM_VOTES = 3
+
 	def hackday_params
 		params.require(:hackday).permit(:name)
 	end
+
+	def get_num_votes(hackday)
+
+	end
+
+	def decrease_votes(hackday)
+
+	end
+
 end
